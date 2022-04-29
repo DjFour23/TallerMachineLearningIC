2bcf58d38aadffe312608aa6413325a8795a4b03
Diego Benavides <diegojose2322@hotmail.com>
2022-04-29 00:43:37 -0500

    Aplicacion del tercer algoritmo al modelo y se crean sus metricas

--- Bank Marketing/MCBankMarketing.py
+++ Bank Marketing/MCBankMarketing.py
@@ -86,3 +86,25 @@ print(f'accuracy de Test de Entrenamiento: {svc.score(x_test, y_test)}')
 # Accuracy de Validación
 print(f'accuracy de Validación: {svc.score(x_test_out, y_test_out)}')
 
+# ARBOL DE DECISIÓN
+
+# Seleccionar un modelo
+arbol = DecisionTreeClassifier()
+
+# Entreno el modelo
+arbol.fit(x_train, y_train)
+
+# MÉTRICAS
+
+print('*'*50)
+print('Decisión Tree')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {arbol.score(x_train, y_train)}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {arbol.score(x_test, y_test)}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {arbol.score(x_test_out, y_test_out)}')
+
