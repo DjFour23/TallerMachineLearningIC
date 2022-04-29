526bf62cef1f6408a4e5605f79f85907e9b922d6
Diego Benavides <diegojose2322@hotmail.com>
2022-04-29 00:38:46 -0500

    Aplicacion del segundo algoritmo al modelo y se crean sus metricas

--- weatherAUS/MCWatherAUS.py
+++ weatherAUS/MCWatherAUS.py
@@ -55,3 +55,25 @@ print(f'accuracy de Test de Entrenamiento: {logreg.score(x_test, y_test)}')
 
 # Accuracy de Validación
 print(f'accuracy de Validación: {logreg.score(x_test_out, y_test_out)}')
+
+# MAQUINA DE SOPORTE VECTORIAL
+
+# Seleccionar un modelo
+svc = SVC(gamma='auto')
+
+# Entreno el modelo
+svc.fit(x_train, y_train)
+
+# MÉTRICAS
+
+print('*'*50)
+print('Maquina de soporte vectorial')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {svc.score(x_train, y_train)}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {svc.score(x_test, y_test)}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {svc.score(x_test_out, y_test_out)}')
